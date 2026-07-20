144b74d081576c62df374f9b19022c71ebe4b759
Dhanush K <dhanushkesavaraj.k.dev@gmail.com>
2026-07-20 23:40:56 +0530

    Add book.py file

---
+++ book.py
@@ -0,0 +1,9 @@
+# This file contains book class and its operation.
+
+class Book :
+    # constructor
+    def __init__(self,book_id,title,author,total_copies):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.total_copies = total_copies
